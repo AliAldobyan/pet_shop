2db0711a4ec18b658fb45aed4c2e956ee3ef558a
jawish-a@outlook.com <jawish-a@outlook.com>
2020-09-06 11:26:38 +0300

    fixed urls and settings for pet app

--- pet_shop/urls.py
+++ pet_shop/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from pet.views import list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('list/', list_view, name='pet-list')
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
